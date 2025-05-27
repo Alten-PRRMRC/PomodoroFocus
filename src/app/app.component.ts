53efa9165f0cecbadd08eaf26a396704116b9f63
mperrelli <a@a.com>
2025-05-27 13:15:54 +0200

    Update responsive component and app styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { ResponsiveComponent } from "./shared/responsive/responsive.component";
+import { TimerComponent } from "../timer/timer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [ResponsiveComponent, TimerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
